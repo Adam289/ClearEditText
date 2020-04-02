e3adaa435497a5308c26b0d40956b3b0a604fd46
Van-Ming <646102528@qq.com>
2020-04-02 16:35:14 +0800

    init project

---
+++ app/src/main/java/com/adam/clearedittext/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.adam.clearedittext
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @Description test the EditTextClear
+ * @Author adam289
+ * @Date 2020/4/2
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
